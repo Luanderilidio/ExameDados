101975443a0c2bd788baeeb9047fb9a46188188f
luanderilidio <luanderilidio174@gmail.com>
2024-01-20 07:12:30 -0400

    feat: ajusta fundo grade

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -3,12 +3,14 @@ import { Session1 } from "../../components/Sessions/Session-1";
 import { Header } from "../../components/Header";
 import { Base } from "../../components/Base";
 import { Session2 } from "../../components/Sessions/Session-2";
+import Background from '../../assets/backgroundGrid.svg'
+
 
 export default function Home() {
   return (
     <div
       className="bg-cover bg-no-repeat "
-      style={{ backgroundImage: 'url("src/assets/backgroundGrid.svg")' }}
+      style={{ backgroundImage: `url(${Background})` }}
     >
       <Header />
       <Session1 />
